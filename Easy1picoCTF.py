96381a212c3a94f9099c10057f66f9b924378079
Mario J <mjere003@gmail.com>
2020-12-02 02:17:59 -0500

    Update Easy1picoCTF.py

--- Easy1picoCTF.py
+++ Easy1picoCTF.py
@@ -12,8 +12,7 @@ solvedFlag = []
 for v,k in zip(encryptedFlag, key):
     sol = alphabet[alphabet.index(v)-alphabet.index(k)]
     solvedFlag.append(sol)
-    print(alphabet.index(v)-alphabet.index(k))
-
+    
 print("picoCTF{" + ''.join(solvedFlag) + "}")
 
 
